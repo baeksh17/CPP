29c3512e6583d1fddac523b90cbcadc3fc11bdfc
白昔虎 <bko117@shiromukashitoranoMacBook-Pro.local>
2018-01-20 19:12:27 +0900

    Poco_Uri_Edit create

--- Poco_Uri_Edit/Poco_Uri_Edit/main.cpp
+++ Poco_Uri_Edit/Poco_Uri_Edit/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  Poco_Json_Convert_2
+//  Poco_Uri_Edit
 //
 //  Created by 白昔虎 on 2018/01/20.
 //  Copyright © 2018年 BAEK SEOKHO. All rights reserved.
@@ -10,6 +10,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    
+    std::cout << "Hello, World!\n";
     return 0;
 }
