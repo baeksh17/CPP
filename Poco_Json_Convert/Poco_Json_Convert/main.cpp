a52fc6a1355e56acfa3115c9ee309f1f9836e756
白昔虎 <bko117@shiromukashitoranoMacBook-Pro.local>
2018-01-20 09:53:41 +0900

    Poco_Json_Convert Project Create

---
+++ Poco_Json_Convert/Poco_Json_Convert/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Poco_Json_Convert
+//
+//  Created by 白昔虎 on 2018/01/20.
+//  Copyright © 2018年 BAEK SEOKHO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
