4dde519c306686789ec29601d397747adc0e808b
白昔虎 <bko117@shiromukashitoranoMacBook-Pro.local>
2018-01-24 01:31:58 +0900

    20170123

---
+++ Poco_Http_server/Poco_Http_server/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Poco_Http_server
+//
+//  Created by 白昔虎 on 2018/01/22.
+//  Copyright © 2018年 BAEK SEOKHO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Poco_Https_Client/Poco_Https_Client/main.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include "Poco/URI.h"
+#include "Poco/Net/HTTPSClientSession.h"
+#include "Poco/Net/HTTPRequest.h"
+#include "Poco/Net/HTTPResponse.h"
+#include "Poco/StreamCopier.h"
+#include "Poco/Net/Context.h"
+#include "Poco/Net/SSLManager.h"
+#include "Poco/Net/AcceptCertificateHandler.h"
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    
+    // SSL接続情報 初期化
+    Poco::Net::initializeSSL();
+    Poco::Net::SSLManager::InvalidCertificateHandlerPtr ptrHandler ( new Poco::Net::AcceptCertificateHandler(false) );
+    Poco::Net::Context::Ptr ptrContext ( new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, "") );
+    Poco::Net::SSLManager::instance().initializeClient(0, ptrHandler, ptrContext);
+    
+    
+    try {
+        Poco::URI uri("https://localhost:9443");
+        Poco::Net::HTTPSClientSession session(uri.getHost(), 9443);
+        Poco::Net::HTTPRequest req("POST", "https://localhost:9443", Poco::Net::HTTPMessage::HTTP_1_1);
+        
+        // リクエスト送信
+        ostream& ss = session.sendRequest(req);
+        
+        // レスポンス受信
+        Poco::Net::HTTPResponse res;
+        istream& rs = session.receiveResponse(res);
+        
+        stringstream sstr;
+        Poco::StreamCopier::copyStream(rs, sstr);
+        string response = sstr.str();
+        cout << string("response:") + response << endl;
+    }
+    catch ( Poco::Exception& ex )
+    {
+        string msg = string("Poco Exception : ") + ex.what() + ", message: " + ex.message();
+        cout << msg << endl;
+    }
+    
+    return 0;
+}
